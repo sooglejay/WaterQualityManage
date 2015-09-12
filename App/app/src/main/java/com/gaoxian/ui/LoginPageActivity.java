56dd5632086d1fce0205fcf049c9303b2b827da7
liuliu <1924313129@qq.com>
2015-09-12 17:08:22 +0800

    搭建了登录和欢迎界面

---
+++ App/app/src/main/java/com/gaoxian/ui/LoginPageActivity.java
@@ -0,0 +1,19 @@
+package com.gaoxian.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.gaoxian.R;
+
+/**
+ * Created by ll on 2015/9/11.
+ */
+public class LoginPageActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loginpage);
+
+    }
+}
